98c9d7f32f89aa412de89c5e6a6dd3193f4a7e93
Ashik Billah Walid <20101111@uap-bd.edu>
2022-01-30 18:35:00 +0600

    Add solution for problem `A - Not Overflow` Link: https://atcoder.jp/contests/abc237/tasks/abc237_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc237/submissions/28929045

---
+++ atcoder/abc237/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin>>n;
+    if(n>=-pow(2,31) && n<pow(2,31)){
+        cout<<"Yes"<<endl;
+    }
+     else cout<<"No"<<endl;
+
+
+
+}
