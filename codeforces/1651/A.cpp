8e04fad0b0b0cbc8c30146e5035657ebc1c400b9
Ashik Billah Walid <20101111@uap-bd.edu>
2022-03-10 20:53:00 +0600

    Add solution for problem `A - Playoff` Link: https://codeforces.com/contest/1651/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1651/submission/149113358

---
+++ codeforces/1651/A.cpp
@@ -0,0 +1,38 @@
+///*BISMILLAHIR RAHMANIR RAHIM*///
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define MP make_pair
+#define endl '\n'
+#define rep(i,n) for(int i=0;i<n;i++)
+#define rep1(i,n) for(int i=1;i<=n;i++)
+#define per(i,n) for(int i=n-1;i>=0;i--)
+#define per1(i,n) for(int i=n;i>=1;i--)
+#define optimize() ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define max_size 1000001
+using namespace std;
+typedef vector<int> VI;
+typedef vector<long long int> VLL;
+// freopen("input.txt","r",stdin);
+// freopen("output.txt","w",stdout);
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    long long  t;
+    cin>>t;
+    while(t--)
+    {
+        long long  n;
+        cin>>n;
+        long long x=pow(2,n);
+
+            cout<<x-1<<endl;
+        }
+
+    return 0;
+}
