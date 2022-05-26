634101a88cca0de0f3d637dfc37f7e0dd73232d0
Ashik Billah Walid <20101111@uap-bd.edu>
2022-05-26 23:07:00 +0600

    Add solution for problem `B - Z mod X = C` Link: https://codeforces.com/contest/1684/problem/B Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1684/submission/158552010

---
+++ codeforces/1684/B.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 10000000
+int main()
+{
+    long long t;
+    cin>>t;
+    while(t--)
+    {
+        long long  a,b,c;
+        cin>>a>>b>>c;
+        cout<<a+b+c<<" "<<b+c<<" "<<c<<endl;
+
+    }
+    return 0;
+}
