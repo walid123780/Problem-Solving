7a750f280f296ec524ddabd2be17613e700876f7
Ashik Billah Walid <20101111@uap-bd.edu>
2022-09-14 11:39:00 +0600

    Add solution for problem `A - Soldier and Bananas` Link: https://codeforces.com/contest/546/problem/A Tags: brute force, implementation, math, *800 Ref: https://codeforces.com/contest/546/submission/172104441

---
+++ codeforces/546/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll             long long int
+#define pii            pair<string,int>
+#define rep(i,n)       for(int i=0;i<n;i++)
+#define rep1(i,n)      for(int i=1;i<=n;i++)
+#define output(a,n)    for(int i=0;i<n;i++) cout<<a[i]<<" ";
+#define pb             push_back
+#define fast           ios_base::sync_with_stdio(false);
+#define optimize()     ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    fast;
+    ll k, w , n;
+    ll ans = 0;
+    cin>>k>>w>>n;
+    ll res =  max(0LL , n * (n+1) / 2 * k - w);
+    cout<<res<<endl;
+
+    return 0;
+}
