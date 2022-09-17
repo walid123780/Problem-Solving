bb98aafe4b84694ade841eda6b8ce9633868c99f
Ashik Billah Walid <20101111@uap-bd.edu>
2022-09-17 18:03:00 +0600

    Add solution for problem `A - Anyway Takahashi` Link: https://atcoder.jp/contests/abc269/tasks/abc269_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc269/submissions/34922808

---
+++ atcoder/abc269/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll             long long int
+#define pii            pair<string,int>
+#define rep(i,n)       for(int i=0;i<n;i++)
+#define cmp(i,n)       for(int i=1;i<n;i++)
+#define rep1(i,n)      for(int i=1;i<=n;i++)
+#define output(a,n)    for(int i=0;i<n;i++) cout<<a[i]<<" ";
+#define pb             push_back
+#define fast           ios_base::sync_with_stdio(false);
+#define optimize()     ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+int main()
+{
+    fast;
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<(a+b)*(c-d) <<endl;
+    cout<<"Takahashi\n";
+
+    return 0;
+}
