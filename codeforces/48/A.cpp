f0ca6a40146d141131e62e079539f11b074defc5
Ashik Billah Walid <20101111@uap-bd.edu>
2022-09-18 18:06:00 +0600

    Add solution for problem `A - Rock-paper-scissors` Link: https://codeforces.com/contest/48/problem/A Tags: implementation, schedules, *900 Ref: https://codeforces.com/contest/48/submission/172556337

--- codeforces/48/A.cpp
+++ codeforces/48/A.cpp
@@ -10,31 +10,40 @@ using namespace std;
 #define fast           ios_base::sync_with_stdio(false);
 #define optimize()     ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
-//bool wins(string s, string t)
-//{
-//    if(s == "rock") return t == "scissors";
-//    if(s == "scissors") return t == "paper";
-//    if(s == "paper") return t == "rock";
-//
-//}
+bool wins(string s, string t)
+{
+    if(s == "rock") return t == "scissors";
+    if(s == "scissors") return t == "paper";
+    if(s == "paper") return t == "rock";
+
+}
 int main()
 {
     fast;
 //    optimize();
-    int a[3] = {0};
-    vector<string>s(3);
-    rep(i,3) cin>>s[i];
+    string arr = "FMS";
+    vector<string>v(3);
+    rep(i,3) cin>>v[i];
     rep(i,3)
     {
         bool ok = true;
         rep(j,3)
         {
-            if( (s[i][0] == 'r' && s[j][0] == 's') || (s[i][0] == 's' && s[j][0] == 'p') || (s[i][0] == 'p' && s[j][0] == 'r')) a[i]++;
+            if(i != j)
+            {
+                if(!wins(v[i],v[j]))
+                {
+                    ok = false;
+                    break;
+                }
+            }
         }
+        if(ok == true)
+            {
+                cout<<arr[i]<<"\n";
+                return 0;
+            }
     }
-    if(a[0] == 2) cout<<"F"; else
-    if(a[1] == 2) cout<<"M"; else
-    if(a[2] == 2) cout<<"S"; else
-    cout<<"?";
+    cout<<"?\n";
     return 0;
 }
