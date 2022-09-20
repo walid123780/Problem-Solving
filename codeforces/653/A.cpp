e3595206ec33176de121c357fdce46b9315af14a
Ashik Billah Walid <20101111@uap-bd.edu>
2022-09-20 08:00:00 +0600

    Add solution for problem `A - Bear and Three Balls` Link: https://codeforces.com/contest/653/problem/A Tags: brute force, implementation, sortings, *900 Ref: https://codeforces.com/contest/653/submission/172775467

--- codeforces/653/A.cpp
+++ codeforces/653/A.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define pii            pair<string,int>
 #define rep(i,n)       for(int i=0;i<n;i++)
 #define rep1(i,n)      for(int i=1;i<=n;i++)
-#define output(a,n)    for(int i=0;i<n;i++) cout<<a[i]<<" ";
+#define output(a,n)    for(int i=1;i<=n;i++) cout<<a[i]<<" ";
 #define pb             push_back
 #define fast           ios_base::sync_with_stdio(false);
 #define optimize()     ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
@@ -17,15 +17,16 @@ int main()
 //    optimize();
     int n;
     cin>>n;
-    int arr[n+1];
-    rep(i,n) cin>>arr[i];
-    sort(arr,arr+n);
-    n = unique(arr,arr + n) - arr;
+    int arr[1000 + 7] = {0};
     rep(i,n)
     {
-        if(arr[i+1] - arr[i] == 1 && arr[i+2] - arr[i+1] == 1 && i + 2 < n)
-        {
-            cout<<"YES";
+        int x;
+        cin>>x;
+        arr[x] = 1;
+    }
+    for(int i = 1; i <= 1000; i++){
+        if(arr[i] && arr[i+1] && arr[i+2]){
+            cout<<"YES\n";
             return 0;
         }
     }
