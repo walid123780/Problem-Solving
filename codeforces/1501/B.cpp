d3eefa43434383940f27d464e72988511d75b00b
Ashik Billah Walid <20101111@uap-bd.edu>
2022-09-27 19:09:00 +0600

    Add solution for problem `B - Napoleon Cake` Link: https://codeforces.com/contest/1501/problem/B Tags: dp, implementation, sortings, *900 Ref: https://codeforces.com/contest/1501/submission/173706827

---
+++ codeforces/1501/B.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll             long long int
+#define pii            pair<int,int>
+#define rep(i,n)       for(int i=0;i<n;i++)
+#define rep1(i,n)      for(int i=1;i<=n;i++)
+#define output(a,n)    for(int i=0;i<n;i++) cout<<a[i]<<" ";
+#define pb             push_back
+#define nn             "\n"
+#define fast           ios_base::sync_with_stdio(false);
+#define optimize()     ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+int main()
+{
+    fast;
+//    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,value = 0,mn =INT_MAX;
+        cin>>n;
+        int x[n+3] = {0};
+        for(int i = 0; i < n; i++)
+        {
+            int z ;
+            cin>>z;
+            x[i+1] += -1;
+            x[max(0,i-z+1)] += 1;
+        }
+
+        ll c = 0;
+        for(int i = 0 ; i < n; i++)
+        {
+            c+=x[i];
+            if(c == 0) cout<<0<<" ";
+            else cout<<1<<" ";
+        }
+        cout<<nn;
+    }
+    return 0;
+
+}
