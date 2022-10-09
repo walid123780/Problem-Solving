e15dcee3ea3429f5236a9944049332319329b24b
Ashik Billah Walid <20101111@uap-bd.edu>
2022-10-09 23:10:00 +0600

    Add solution for problem `A - Image` Link: https://codeforces.com/contest/1721/problem/A Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1721/submission/175269486

---
+++ codeforces/1721/A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll            long long int
+#define pii            pair<int,int>
+#define rep(i,n)       for(int i=0;i<n;i++)
+#define rep1(i,n)      for(int i=1;i<=n;i++)
+#define output(a,n)    for(int i=0;i<n;i++) cout<<a[i]<<" ";
+#define pb             push_back
+#define str_unique     s.resize(unique(s.begin(), s.end()) - s.begin());
+#define str_unique1    s.erase(unique(s.begin(),s.end()),s.end());
+#define ll_length(x)   to_string(x).length()
+#define fast           ios_base::sync_with_stdio(false);
+#define optimize()     ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    fast;
+//    optimize();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        set<char>p;
+         for(int i = 0; i < 2; i++)
+        {
+            string s;
+            cin>>s;
+            for(int j = 0; j < 2; j++){
+                p.insert(s[j]);
+            }
+        }
+        cout<<p.size()-1<<endl;
+    }
+    return 0;
+}
