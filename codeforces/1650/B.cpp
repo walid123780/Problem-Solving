5b87387018c87deffa29f62d8dc257916f194ddf
Ashik Billah Walid <20101111@uap-bd.edu>
2022-10-21 18:06:00 +0600

    Add solution for problem `B - DIV + MOD` Link: https://codeforces.com/contest/1650/problem/B Tags: math, *900 Ref: https://codeforces.com/contest/1650/submission/177301585

---
+++ codeforces/1650/B.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll             long long int
+#define pii            pair<int,int>
+#define rep(i,n)       for(ll i=0;i<n;i++)
+#define rep1(i,n)      for(int i=1;i<=n;i++)
+#define output(a,n)    for(int i=0;i<n;i++) cout<<a[i]<<" ";
+#define pb             push_back
+#define fast           ios_base::sync_with_stdio(false);
+#define optimize()     ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define str_unique     s.resize(unique(s.begin(), s.end()) - s.begin());
+#define str_unique1    s.erase(unique(s.begin(),s.end()),s.end());
+#define int_length(x)  to_string(x).length()
+
+void solve()
+{
+    ll l,r,a, md = -1, oc = -1,ans = -1, up =  -1,mod = -1;
+    cin>>l>>r>>a;
+    up = r / a;
+    mod = r % a;
+    ans = up+mod;
+    ll check = (up * a) - 1;
+    if(check >= l)
+    {
+        oc = check / a;
+        md = check % a;
+    }
+
+    ans = max(ans, (oc + md));
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    fast;
+//  optimize();
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
