fa3d8ecfa9ef7c713398ba97630b1a94096d052d
Ashik Billah Walid <20101111@uap-bd.edu>
2022-11-09 12:16:00 +0600

    Add solution for problem `B - Letter` Link: https://codeforces.com/contest/43/problem/B Tags: implementation, strings, *1100 Ref: https://codeforces.com/contest/43/submission/180160481

--- codeforces/43/B.cpp
+++ codeforces/43/B.cpp
@@ -7,7 +7,6 @@ using namespace std;
 #define rep1(i,n)      for(int i=1;i<=n;i++)
 #define output(a,n)    for(int i=0;i<n;i++) cout<<a[i]<<" ";
 #define pb             push_back
-#define sentence(s)       scanf(" %[^\n]s\n",s);
 #define YES            cout << "YES\n"
 #define NO             cout << "NO\n"
 #define fast           ios_base::sync_with_stdio(false);
@@ -18,27 +17,28 @@ using namespace std;
 
 void solve()
 {
-    char s[200], s1[201];
-    sentence(s);
-    sentence(s1);
+    char s[5000], s1[5000];
+    fgets(s, 5000, stdin);
+    fgets(s1, 5000, stdin);
 
     multiset<char>a;
     for(int i = 0 ; s[i] != '\0'; i++)
     {
         a.insert(s[i]);
     }
+
     for(int i = 0; i < s1[i] != '\0'; i++)
     {
 
         auto it = a.find(s1[i]);
         if(it == a.end() )
         {
-            NO;
+            cout<<"NO\n";
             return ;
         }
         else if(it != a.end() && s1[i] != ' ')a.erase(it);
     }
-    YES;
+    cout<<"YES\n";
 
 }
 int main()
