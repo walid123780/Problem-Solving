7f5dd125b5d6ed4f25359f58bb8c98df2407000e
Ashik Billah Walid <20101111@uap-bd.edu>
2023-03-06 17:51:00 +0600

    Add solution for problem `B - K-th Beautiful String` Link: https://codeforces.com/contest/1328/problem/B Tags: binary search, brute force, combinatorics, implementation, math, *1300 Ref: https://codeforces.com/contest/1328/submission/196248919

--- codeforces/1328/B.cpp
+++ codeforces/1328/B.cpp
@@ -2,36 +2,44 @@
 using namespace std;
 
 #define ll long long
-int f(ll n){
-    ll temp = n * (n + 1);
-    return temp / 2;
-}
 int main()
 {
+
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+
     int t;
     cin>>t;
     while(t--)
     {
-        int n , k ;
-        cin>>n>>k;
-        string s = "";
-        for(int i = 0 ; i < n ; i++){
-            s += 'a';
+        long long a, b;
+        cin>>a>>b;
+        long long gen[a + 1] = {0};
+        for(ll i = 1  ; i < a; i++)
+        {
+            gen[i] = i;
+            if(i != 1) gen[i] = gen[i] + gen[i - 1];
         }
-        ll i =  1 , up;
-        int fB = n - 2 , SB = n - 1;
-        for(; ; i++){
-            up = f(i);
-            if(up >= k) break;
-            fB--;
-            SB--;
+        int index = lower_bound(gen, gen + a, b) - gen;
+        if(gen[index]  != b) index -= 1;
+        long long kothai =  gen[index] - gen[index - 1];
+        map<int, int > mp;
+        mp[a - kothai] = 1;
+        mp[a - (kothai + 1)] = 1;
+        if(gen[index] != b)
+        {
+            mp[a - kothai] = 0;
+            mp[a - (kothai + 1)] = 0;
+            mp[a - (kothai + 2)] = 1;
+            long long k = b - gen[index];
+            mp[a - k] = 1;
         }
-        ll msb = up - k;
-        s[fB] = 'b';
-        s[msb + SB] = 'b';
-        cout<<s<<endl;
+        for(int i =0 ; i < a ; i++)
+        {
+            if(mp[i] != 1) cout<<'a';
+            else cout<<'b';
+        }
+        cout<<endl;
     }
     return 0;
 }
