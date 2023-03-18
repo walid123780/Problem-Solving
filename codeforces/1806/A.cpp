c0d1c1f3aab1752afe835f64bf8b388b49a0960f
Ashik Billah Walid <20101111@uap-bd.edu>
2023-03-18 18:55:00 +0600

    Add solution for problem `A - Walking Master` Link: https://codeforces.com/contest/1806/problem/A Tags: geometry, greedy, math, *800 Ref: https://codeforces.com/contest/1806/submission/197937178

--- codeforces/1806/A.cpp
+++ codeforces/1806/A.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+#define ll long long
 int main()
 {
 	ios_base::sync_with_stdio(0);
@@ -9,15 +9,15 @@ int main()
 	int t;
 	cin>>t;
 	while(t--){
-		int a , b , c, d , ans = 0;
+		ll a , b , c, d , ans = 0;
 		cin >> a >> b >> c >> d;
 		if(b > d) {cout << -1 << endl; continue;}
-		int f = (d) - (b);
+		ll f = (d) - (b);
 		a += f;
-		b += f;
+		b = d;
 		if((c - a) > 0) {cout << -1 << endl; continue;}
 		else{
-			ans = abs(c - a) + f;
+			ans = abs((c) - (a)) + f;
 			cout << ans << endl;
 		}
 	}
