e1e343fedd564dcb1b824b4cab764474779e3443
Ashik Billah Walid <20101111@uap-bd.edu>
2023-03-30 07:37:00 +0600

    Add solution for problem `B - Tanya and Candies` Link: https://codeforces.com/contest/1118/problem/B Tags: implementation, *1200 Ref: https://codeforces.com/contest/1118/submission/199771836

--- codeforces/1118/B.cpp
+++ codeforces/1118/B.cpp
@@ -4,33 +4,33 @@ using namespace std;
 #define ll long long
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    int t = 1;
-        // cin>>t;
-    while(t--){
-        int n;
-        cin >> n;
-        std::vector<ll>  o_pre(n+1, 0) , e_pre(n+1, 0);
-        ll odd = 0 , even = 0;
-        for(int i = 1; i <= n; i++){
-            int x;
-            cin >> x;
-            if(i % 2) o_pre[i] += x;
-            else e_pre[i] += x;
-        }
-        
-        for(int i = 1; i <= n; i++){
-            o_pre[i] += o_pre[i - 1];
-            e_pre[i] += e_pre[i - 1];
-        }
-        int ans = 0;
-        for(int i = 1; i <= n ; i++){
-            odd = o_pre[i - 1] + e_pre[n] - e_pre[i];
-            even = e_pre[i - 1] + o_pre[n] - o_pre[i];
-            if(odd == even) ans += 1;
-        }
-        cout << ans << endl;
-    }
-    return 0;//
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+		// cin>>t;//
+	while(t--){
+		int n;
+		cin >> n;
+		std::vector<ll>  o_pre(n+1, 0) , e_pre(n+1, 0);
+		ll odd = 0 , even = 0;
+		for(int i = 1; i <= n; i++){
+			int x;
+			cin >> x;
+			if(i % 2) o_pre[i] += x;
+			else e_pre[i] += x;
+		}
+		
+		for(int i = 1; i <= n; i++){
+			o_pre[i] += o_pre[i - 1];
+			e_pre[i] += e_pre[i - 1];
+		}
+		int ans = 0;
+		for(int i = 1; i <= n ; i++){
+			odd = o_pre[i - 1] + e_pre[n] - e_pre[i];
+			even = e_pre[i - 1] + o_pre[n] - o_pre[i];
+			if(odd == even) ans += 1;
+		}
+		cout << ans << endl;
+	}
+	return 0;
 }
