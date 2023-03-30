62856af1c41ed643fb5dabf3250c810dbf7e9870
Ashik Billah Walid <20101111@uap-bd.edu>
2023-03-30 06:35:00 +0600

    Add solution for problem `B - Tanya and Candies` Link: https://codeforces.com/contest/1118/problem/B Tags: implementation, *1200 Ref: https://codeforces.com/contest/1118/submission/199769184

--- codeforces/1118/B.cpp
+++ codeforces/1118/B.cpp
@@ -11,24 +11,31 @@ int main()
 	while(t--){
 		int n;
 		cin >> n;
-		std::vector<ll>  o_pre(n+1, 0) , e_pre(n+1, 0);
-		ll odd = 0 , even = 0;
-		for(int i = 1; i <= n; i++){
-			int x;
-			cin >> x;
-			if(i % 2) o_pre[i] += x;
-			else e_pre[i] += x;
+		std::vector<ll> v(n) , pre(n , 0) , o_pre(n , 0) , e_pre(n , 0);
+		for(int i = 0; i < n; i++){
+			cin >> v[i];
+			pre[i] = v[i];
+			if(i) pre[i] = pre[i - 1] + pre[i];
 		}
-		
-		for(int i = 1; i <= n; i++){
-			o_pre[i] += o_pre[i - 1];
-			e_pre[i] += e_pre[i - 1];
+		o_pre[0] = v[0];
+		e_pre[0] = 0;
+		if(n >= 1) e_pre[1] = v[1];
+		for(int i = 0; i < n; i++){
+			if(i % 2) o_pre[i] = o_pre[i - 1]; 
+			else if(!(i % 2) && i >= 2) o_pre[i] = v[i] + o_pre[i - 2];
+			if(!(i % 2) && i) e_pre[i] = e_pre[i-1];
+			else if(i % 2 &&  i >= 3) e_pre[i] = e_pre[i- 2] + v[i];
 		}
 		int ans = 0;
-		for(int i = 1; i <= n ; i++){
-			odd = o_pre[i - 1] + e_pre[n] - e_pre[i];
-			even = e_pre[i - 1] + o_pre[n] - o_pre[i];
-			if(odd == even) ans += 1;
+		for(int i = 0 ; i < n; i++){
+			ll sum = 0, o_sum = 0, e_sum = 0;
+			sum = pre[n - 1] - pre[i];
+			if(i % 2){o_sum = e_pre[n-1] - e_pre[i]; e_sum = sum - o_sum;} 
+			else {e_sum = o_pre[n-1] - o_pre[i]; o_sum = sum - e_sum;}
+			if(i)e_sum += e_pre[i - 1];
+			if(i)o_sum += o_pre[i - 1];
+			// cout << e_sum << ' ' << o_sum << endl;
+			if(e_sum == o_sum) ans += 1;
 		}
 		cout << ans << endl;
 	}
