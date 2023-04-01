17b2ce65368f4b618f3851f932d1fdce51572b39
Ashik Billah Walid <20101111@uap-bd.edu>
2023-04-01 19:27:00 +0600

    Add solution for problem `D2 - Coffee and Coursework (Hard Version)` Link: https://codeforces.com/contest/1118/problem/D2 Tags: binary search, greedy, *1700 Ref: https://codeforces.com/contest/1118/submission/200127295

---
+++ codeforces/1118/D2.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    while(t--){
+    	int n , page;
+    	cin >> n >> page;
+    	std::vector<int> v(n);
+    	int sum = 0;
+    	for(int i = 0; i < n; i++){
+    		cin >> v[i];
+    	}
+    	sort(v.begin(), v.end() , greater<int>());
+    	int l = 1, r = n, ans = -1;
+    	while(l <= r){
+    		int mid = l + r >> 1;
+            ll sum = 0;
+    		for(int j = 0 ; j < n ; j++){
+    			sum += max(v[j] - j / mid, 0);
+    		}
+    		if(sum >= page){
+                ans =  mid;
+                r = mid - 1;
+            }	
+            else l =  mid + 1;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
