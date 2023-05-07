692ce590502830b0fbd6144ae30d109c29da7e53
Ashik Billah Walid <20101111@uap-bd.edu>
2023-05-07 14:21:00 +0600

    Add solution for problem `F - Forever Winter` Link: https://codeforces.com/contest/1829/problem/F Tags: dfs and similar, graphs, math, *1300 Ref: https://codeforces.com/contest/1829/submission/204944121

--- codeforces/1829/F.cpp
+++ codeforces/1829/F.cpp
@@ -8,45 +8,45 @@ int main()
    int t;
    cin >> t;
    while(t--) {
-	  int n , m;
-	  cin >> n >> m;
-	  std::vector<int> g[N];
-	  for(int i = 1; i <= m; i++) {
-		 int u, v;
-		 cin >> u >> v;
-		 g[u].push_back(v);
-		 g[v].push_back(u);
-	  }
-	  int dist[N]; int vis[N];
-	  for(int i = 1 ; i <= n; i++) {
-		 memset(dist , 0 , sizeof dist);
-		 memset(vis , 0 , sizeof vis);
-		 int mx = 0;
-		 vector<int> ans;
-		 queue<int> q;
-		 q.push(i);
-		 vis[i] = 1;
-		 dist[i] = 0;
-		 while(!q.empty()) {
-			int u = q.front();
-			q.pop();
-			int cnt = 0;
-			for(auto v : g[u])  {
-			   if(!vis[v]) {
-				  dist[v] = dist[u] + 1;
-				  vis[v] = true;
-				  q.push(v);
-				  cnt ++;
-				  mx = max(mx , dist[v]);
-			   }  
-			}
-			if(cnt) ans.push_back (cnt);
-		 }
-		 if(mx == 2) {
-			cout << ans[0]<< " "  << ans[1] << endl;
-			break;
-		 }
-	  }
+      int n , m;
+      cin >> n >> m;
+      std::vector<int> g[N];
+      for(int i = 1; i <= m; i++) {
+         int u, v;
+         cin >> u >> v;
+         g[u].push_back(v);
+         g[v].push_back(u);
+      }
+      int dist[N]; int vis[N];
+      for(int i = 1 ; i <= n; i++) {
+         memset(dist , 0 , sizeof dist);
+         memset(vis , 0 , sizeof vis);
+         int mx = 0;
+         vector<int> ans;
+         queue<int> q;
+         q.push(i);
+         vis[i] = 1;
+         dist[i] = 0;
+         while(!q.empty()) {
+            int u = q.front();
+            q.pop();
+            int cnt = 0;
+            for(auto v : g[u])  {
+               if(!vis[v]) {
+                  dist[v] = dist[u] + 1;
+                  vis[v] = true;
+                  q.push(v);
+                  cnt ++;
+                  mx = max(mx , dist[v]);
+               }  
+            }
+            if(cnt) ans.push_back (cnt);
+         }
+         if(mx == 2) {
+            cout << ans[0]<< " "  << ans[1] << endl;
+            break;
+         }
+      }
    }
    return 0;
 }
