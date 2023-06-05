b3f67e484748b93dd207436edb68f487e40e2332
Ashik Billah Walid <20101111@uap-bd.edu>
2023-06-05 19:43:00 +0600

    Add solution for problem `C - Computer Game` Link: https://codeforces.com/contest/1183/problem/C Tags: binary search, math, *1400 Ref: https://codeforces.com/contest/1183/submission/208597482

--- codeforces/1183/C.cpp
+++ codeforces/1183/C.cpp
@@ -2,11 +2,6 @@
 using namespace std;
 
 #define ll long long
-bool ok(ll mid ,ll k ,ll n ,ll a, ll b) {
-       ll f = (mid * a) + (n - mid) * b;
-	   if(f < k) return true;
-	   else return false;
-}
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -16,21 +11,21 @@ int main()
     while(t--) {
     	ll k , n , a , b;
     	cin >> k >> n >> a >> b;
-    	
-    	if(k - (n * a) > 0) {
+    	k -= (n * a);
+    	if(k > 0) {
     		cout << n << endl;
     	}
     	else {
-    		int l = 0 , r = n , ans = -1;
-    		while(l <= r) {
-    			ll mid = (l + r) >> 1;
-    			if(ok(mid , k , n , a , b)) {
-    				ans = mid;
-    				l = mid + 1;
-    			}
-    			else r = mid - 1;
+    		k *= -1;
+    		// cout <<  k <<  " ";
+    		ll diff = a - b;
+    		ll turns = (k + diff) / diff;
+    		if(turns > n) {
+    			cout << -1 << endl;
     		}
-    		cout << ans << endl;
+    		else {
+    			cout << n - turns << endl;
+    		}
     	}
     }
     return 0;
