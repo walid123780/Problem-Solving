f00fefca0e7225069f5c0d7856800fba8d87a660
Ashik Billah Walid <20101111@uap-bd.edu>
2023-06-07 11:39:00 +0600

    Add solution for problem `D - Wooden Toy Festival` Link: https://codeforces.com/contest/1840/problem/D Tags: binary search, greedy, sortings, *1400 Ref: https://codeforces.com/contest/1840/submission/208856237

--- codeforces/1840/D.cpp
+++ codeforces/1840/D.cpp
@@ -17,18 +17,30 @@ int main()
             cin >> v[i];   
         }
         sort(v.begin(), v.end());
+        // for(auto it : v) {
+        //     cout << it << " ";
+        // }
+        // cout << endl;
         long long l = 0 , r = MAX , ans = MAX;
         while(l <= r) {
             ll mid = (l + r) >> 1;
             int cnt = 0 , x = mid + v[0];
 
+            std::vector<int> f;
+            f.push_back(v[0]);
             for(int i = 1; i < n; i++) {
                 if(abs(v[i] - x) > mid) {
                      x = mid + v[i];
+                     f.push_back(v[i]);
                      cnt++;
                 }
             }
             if(cnt < 3) {
+                // cout << "MID # : " << mid << " ";
+                // for(auto it : f) {
+                //     cout << it << " ";
+                // }
+                // cout << endl;
                 ans = min(mid , ans);
                 r = mid - 1;
             }
