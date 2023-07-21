296e594230ac2903be9535da786861799a59f612
Ashik Billah Walid <20101111@uap-bd.edu>
2023-07-22 01:42:00 +0600

    Add solution for problem `E - Cardboard for Pictures` Link: https://codeforces.com/contest/1850/problem/E Tags: binary search, geometry, implementation, math, *1100 Ref: https://codeforces.com/contest/1850/submission/214955820

---
+++ codeforces/1850/E.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long 
+typedef __int128 lll;
+const ll MAX = 1e9 + 7;
+lll fun (vector<int> const &a , int w) {
+	lll ans = 0;
+	for(auto it : a) {
+		ans += 1LL * (it + 2 * w) * (it + 2 * w) ;
+	}
+	return ans;
+}
+// I am getting long long overflow in Contest time !!   
+// __int128 actually long long^2 capacity. But we can not use it for input/output(just can store value).
+// __int128 support GNU 18 and upper versions.  
+int main()
+{
+	int t = 1;
+	cin >> t;
+	while(t--) {
+		int n; ll m;
+		cin >> n >> m;
+		std::vector<int> v(n);
+		for(int i = 0; i < n; i++) {
+			cin >> v[i];
+		}
+		int l = 0 , r = MAX;
+		while(l <= r) {
+			ll mid = (l + r) >> 1;
+			if(fun(v , mid) >= m){
+				r = mid - 1;
+			}
+			else l = mid + 1;
+		}
+		cout << l << endl;
+	}
+	return 0;
+}
