263641f99c554c85619dc73221ab109e01a10365
Ashik Billah Walid <20101111@uap-bd.edu>
2023-07-26 15:47:00 +0600

    Add solution for problem `D - Prefix Permutation Sums` Link: https://codeforces.com/contest/1851/problem/D Tags: implementation, math, *1300 Ref: https://codeforces.com/contest/1851/submission/215709734

--- codeforces/1851/D.cpp
+++ codeforces/1851/D.cpp
@@ -5,20 +5,47 @@ using namespace std;
 void solve () {
     ll n;
     cin >> n;
-    std::vector<ll> v(n , 0);
-    map<ll , int > mp;
-    for(int i = 1; i < n; i++) {
-        cin >> v[i];
-        mp[v[i] - v[i - 1]]++;
+    std::vector<ll> v(n - 1);
+    map < ll , int > mp;
+    for(int i = 0; i < n - 1; i++) {
+        cin >> v[i];   
     }
-    int cnt = 0; ll sum = 0;
+    ll nth_sum = (n * (n + 1)) >> 1;
+    long long sum = 0 , missing_sum = 0;
+    for(int i = 1; i < n - 1; i++) {
+        mp[v[i] - v[i - 1]]++;
+        sum += v[i] - v[i - 1];
+    }
+    int missing_element = 0;
     for(int i = 1; i <= n; i++) {
-        if(!mp[i]) cnt += 1 , sum += i;
-        else mp[i] -= 1; 
-    }    
-    if (cnt == 1 and mp[sum] == 0) cout << "YES" << endl;
-    else if (cnt == 2 and mp[sum] == 1) cout << "YES" << endl;
-    else cout << "NO" << endl;
+        missing_element += (mp[i] == 0 ? 1 : 0);
+        missing_sum += (mp[i] == 0 ? i : 0); 
+        sum -= (mp[i] >= 1 ? i : 0);  
+    }   
+    if(v.back() == nth_sum) {
+        if(missing_element == 2 && v[0] == missing_sum)  {
+            cout << "YES" << endl;
+        }
+        else if(missing_element == 3) {
+            if(!mp[v[0]] and v[0] >= 1 && v[0] <= n) {
+                if(mp[sum] >= 1) {
+                    cout << "YES" << endl;
+                }
+            }
+            else cout << "NO" << endl; 
+        }
+        else {
+         cout << "NO" << endl;
+     }
+ } 
+ else if(v.back() != nth_sum) {
+    if(missing_element == 2 and mp[v[0]] == 0 and v[0] >= 1 && v[0] <= n) {
+        cout << "YES" << endl;
+    }
+    else {
+       cout << "NO" << endl;
+   }
+}
 }
 int main() {
     ios_base::sync_with_stdio(0);
