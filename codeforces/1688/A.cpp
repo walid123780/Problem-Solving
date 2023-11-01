f90e10b786e3b22afec46baa01b9fd750347b28b
Ashik Billah Walid <20101111@uap-bd.edu>
2023-11-01 19:41:00 +0600

    Add solution for problem `A - Cirno's Perfect Bitmasks Classroom` Link: https://codeforces.com/contest/1688/problem/A Tags: bitmasks, brute force, *800 Ref: https://codeforces.com/contest/1688/submission/230834384

--- codeforces/1688/A.cpp
+++ codeforces/1688/A.cpp
@@ -1,6 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string convert_base(int n) {
+  int binarynum[65];
+  int i = 0;
+  while (n > 0) {
+    binarynum[i] = n % 2;
+    n = n / 2;
+    i++;
+  }
+  string s = "";
+  for (int j = i - 1; j >= 0; j--)
+    s += binarynum[j] + '0';
+  return s;
+}
+int convert_to_decimal(string s , int base) {
+  int n = 0 , power = 1;
+  for(int i = (int) s.size() - 1; i >= 0 ; i--) {
+    n += power * (s[i] - '0');
+    power *=  base;
+  }
+  return n;
+}
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
@@ -9,12 +30,29 @@ int main() {
   for(int i = 0; i < n; i++) {
     int x;
     cin >> x; 
-    int y = (x & -x);
-    if(x == 1) {
-      cout << 3 << "\n";
-    } 
-    else {
-      cout << y + (x == y) << "\n";
+    string s = convert_base(x);
+    string ans = "";
+    //cout << s << endl;
+    for(int j = s.size() - 1; j >= 0; j--) {
+      if(s[j] == '0') {
+        ans += '0';
+      }
+      else {
+        ans = '1' + ans;
+        break;
+      }
+    }  
+    if(ans == s) {
+      int f = false;
+      for(int j = ans.size() - 1; j > 0; j++) {
+        if(ans[j] == '0') {
+          ans[j] = '1';
+          f = true;
+          break;
+        }
+      }
+      if(!f) ans += '1';
     }
+    cout << convert_to_decimal(ans, 2) << endl;
   }     
 }
