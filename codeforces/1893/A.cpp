ebfecbc433ebb81309e1c12f2d96cdab5338a513
Ashik Billah Walid <20101111@uap-bd.edu>
2023-11-09 14:54:00 +0600

    Add solution for problem `A - Anonymous Informant` Link: https://codeforces.com/contest/1893/problem/A Tags: brute force, dfs and similar, graphs, implementation, *1400 Ref: https://codeforces.com/contest/1893/submission/232041082

--- codeforces/1893/A.cpp
+++ codeforces/1893/A.cpp
@@ -2,34 +2,34 @@
 using namespace std;
 
 bool Anonymous_Informant (std::vector<int> &v, int k, int n) {
-	int start = 0;
-	for(int i = 0; i < k; i++) {
-		int end = (start + n - 1) % n;
-		int val = v[end];
-		if(val > n) {
-			return false;
-		}
-		start = (start - val + n) % n;
-	}
-	return true;
+  int start = 0;
+  for(int i = 0; i < k; i++) {
+   int end = (start + n - 1) % n;
+   int val = v[end];
+   if(val > n) {
+    return false;
+  }
+  start = (start - val + n) % n;
+}
+return true;
 }
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int t = 1;
-	cin >> t;
-	while(t--) {
-		int n, k;
-		cin >> n >> k;
-		std::vector<int> v(n);
-		for(int i = 0; i < n; i++) {
-			cin >> v[i];
-		}
-		int sz = min(n, k);
-		if(Anonymous_Informant(v, sz, n)) {
-			cout << "YES\n";
-		}
-		else cout << "NO\n";
-	} 
-	return 0;
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t = 1;
+  cin >> t;
+  while(t--) {
+    int n, k;
+    cin >> n >> k;
+    std::vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+      cin >> v[i];
+    }
+    int sz = min(n, k);
+    if(Anonymous_Informant(v, sz, n)) {
+      cout << "YES\n";
+    }
+    else cout << "NO\n";
+  } 
+  return 0;
 }
