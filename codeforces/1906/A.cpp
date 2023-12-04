048d055e52cef067ce1af9cd40e2d73ff9fef44b
Ashik Billah Walid <20101111@uap-bd.edu>
2023-12-04 15:43:00 +0600

    Add solution for problem `A - Easy As ABC` Link: https://codeforces.com/contest/1906/problem/A Tags: brute force, *1000 Ref: https://codeforces.com/contest/1906/submission/235687105

--- codeforces/1906/A.cpp
+++ codeforces/1906/A.cpp
@@ -4,29 +4,52 @@ using namespace std;
 const int N = 4;
 string s[N];
 bool vis[N][N];
-vector<string> ans;
-
 int di[] = {0, -1, 0, +1, -1, -1, +1, +1};
 int dj[] = {+1, 0, -1, 0, +1, -1, -1, +1};
 int n = 3, m = 3;
 bool is_valid(int i, int j) {
   return i >= 0 and i < n and j >= 0 and j < m;
 }
-void dfs(int i, int j, int p, string t) {
-  t[p] = s[i][j];
-  if(p == 2) {
-    ans.push_back(t);
-    return;
-  }
-  vis[i][j] = true;
+char minimum (int i, int j) {
+  char mn = 'D'; 
   for (int k = 0; k < 8; k++) {
+    int nxt_i = i + di[k];
+    int nxt_j = j + dj[k];
+    if (is_valid(nxt_i, nxt_j) and !vis[nxt_i][nxt_j]) {
+     mn = min(mn, s[nxt_i][nxt_j]);
+   }
+ }
+ return mn;
+}
+string  dfs(int i, int j) {
+  char find = 'D';
+  vis[i][j] = true;
+  queue<pair<int, int>> q;
+  string ans;
+  ans = s[i][j];
+  q.push({i, j});
+  while (!q.empty()) {
+    auto [i, j] = q.front();
+    q.pop();
+    char mn = minimum(i, j);
+    if(ans.size() == 2) {
+      find = min(mn, find);
+      continue;
+    }
+    else if(ans.size() == 1) {
+      ans += mn;      
+    }
+    for (int k = 0; k < 8; k++) {
       int nxt_i = i + di[k];
       int nxt_j = j + dj[k];
-      if (is_valid(nxt_i, nxt_j) and !vis[nxt_i][nxt_j]) {
-        dfs(nxt_i, nxt_j, p + 1, t);
+      if (is_valid(nxt_i, nxt_j) and s[nxt_i][nxt_j] == mn) {
+        vis[i][j] = true;
+        q.push(make_pair(nxt_i, nxt_j));
       }
     }
-  vis[i][j] = false;
+  }
+  memset(vis, false, sizeof vis);
+  return ans += find;
 }
 int main() {
   ios::sync_with_stdio(0);
@@ -34,13 +57,13 @@ int main() {
   for(int i = 0; i < n; i++) {
     cin >> s[i]; 
   }
+  std::vector<string> answer;
   for(int i = 0; i < n; i++) {
    for(int j = 0; j < n; j++) {
-      memset(vis, false, sizeof vis);
-      dfs(i, j, 0,"abc");
+      answer.push_back(dfs(i, j));
     } 
   }
-  sort(ans.begin(), ans.end());
-  cout << ans.front() << endl;
+  sort(answer.begin(), answer.end());
+  cout << answer.front() << endl;
   return 0;
 }
