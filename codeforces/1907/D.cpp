1ddca638269e518de9d4e2080b8e9b7c674be454
Ashik Billah Walid <20101111@uap-bd.edu>
2023-12-08 12:30:00 +0600

    Add solution for problem `D - Jumping Through Segments` Link: https://codeforces.com/contest/1907/problem/D Tags: binary search, constructive algorithms, *1400 Ref: https://codeforces.com/contest/1907/submission/236292656

--- codeforces/1907/D.cpp
+++ codeforces/1907/D.cpp
@@ -3,39 +3,39 @@ using namespace std;
 
 const int inf = 1e9;
 int main() {
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int t = 1;
-  cin >> t;
-  while(t--) {
-    int n;
-    cin >> n;
-    std::vector<pair<int, int>> v(n);
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	cin >> t;
+	while(t--) {
+		int n;
+		cin >> n;
+		std::vector<pair<int, int>> v(n);
 
-    for(int i = 0; i < n; i++) {
-      cin >> v[i].first >> v[i].second;
-    }
-    int l = 0, r = inf, ans;
-    while(l <= r) {
-      int mid = (l + r) / 2;
-      bool possiable = true; int left = 0, right = 0;
-      for(int i = 0; i < n; i++) {
-        left -= mid;
-        right += mid;
-        if(right < v[i].first || left > v[i].second) {
-          possiable = false;
-          break;
-        }
-        left = max (left, v[i].first);
-        right = min(right, v[i].second);
-      }
-      if(possiable) {
-        ans = mid;
-        r = mid - 1;
-      }
-      else l = mid + 1;
-    }
-    cout << ans << endl;
-  }
-  return 0;
+		for(int i = 0; i < n; i++) {
+			cin >> v[i].first >> v[i].second;
+		}
+		int l = 0, r = inf, ans;
+		while(l <= r) {
+			int mid = (l + r) / 2;
+			bool possiable = true; int left = 0, right = 0;
+			for(int i = 0; i < n; i++) {
+				left -= mid;
+				right += mid;
+				if(right < v[i].first || left > v[i].second) {
+					possiable = false;
+					break;
+				}
+				left = max (left, v[i].first);
+				right = min(right, v[i].second);
+			}
+			if(possiable) {
+				ans = mid;
+				r = mid - 1;
+			}
+			else l = mid + 1;
+		}
+		cout << ans << endl;
+	}
+	return 0;
 }
